7a8136496b326bed7a1d9905f0213b1b96e4ad50
Sham <shamthakare1992@gmail.com>
2021-11-18 00:29:35 +0530

    update data after insert

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -10,6 +10,7 @@ import { EmployeeService } from '../appservices/employee.service';
 export class EmployeeComponent implements OnInit {
   EmpForm: FormGroup;
   kamgardata: any; // 3/10 marks 
+  poupband:boolean=false;
   constructor(private fb: FormBuilder, private api: EmployeeService) {
     this.EmpForm = this.fb.group({
       name: new FormControl('', Validators.required),
@@ -18,19 +19,24 @@ export class EmployeeComponent implements OnInit {
 
     })
   }
-  
+
 
   ngOnInit(): void {
+    this.loadalldata();
+  }
+  loadalldata() {
     this.api.sarvkamgar().subscribe((aalelaData) => {
       if (aalelaData) {
         this.kamgardata = aalelaData;
       }
     })
   }
+
   onSubmit() {
     this.api.addEmployee(this.EmpForm.value).subscribe((res) => {
       if (res) {
         this.EmpForm.reset();
+        this.loadalldata();
       } else {
         alert("not insert")
       }
